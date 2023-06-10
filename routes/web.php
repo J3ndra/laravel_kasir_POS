ea363c9738123f583a6be032817509dca48b1f6c
Endra <endra.kartika.25@gmail.com>
2023-06-10 14:56:19 +0700

    akhir kelas sore

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+use function Termwind\render;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect(route('dashboard.index'));
+});
+
+Route::get('/test', function () {
+    return Inertia::render('index');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::controller(LoginController::class)->group(function () {
+        Route::get('login', 'showLoginForm')->name('login');
+        Route::post('login', 'login');
+
+        Route::middleware(['auth'])->group(function () {
+            Route::post('logout', 'logout')->name('logout');
+        });
+    });
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/', function () {
+            return redirect(route('dashboard.index'));
+        });
+
+        Route::controller(DashboardController::class)->group(function () {
+            Route::get('/dashboard', 'index')->name('dashboard.index');
+        });
+
+        require __DIR__ . '/admin/settings.php';
+    });
+});
